046a164bfec5245f0fb8cc37ab7ba05f8db01ac9
Radmila Gama <milaejared@outlook.com>
2022-04-29 22:28:14 -0300

    Soluções

---
+++ 01 Iniciante/1002 Área do Círculo.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+r = float(input())
+r = 3.14159 * r * r
+print("A={:.4f}".format(r))
---
+++ 01 Iniciante/1003 Soma Simples.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+m = int(input())
+n = int(input())
+print("SOMA =", m+n)
